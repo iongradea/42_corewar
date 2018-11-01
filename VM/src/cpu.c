c5d7e5e71efb0ca01c089019d680659863fcff9e
baptiste bicheron <bbichero@student.42.fr>
2018-11-01 16:30:54 +0100

    merge cycle ps with cycle machine

--- VM/src/cpu.c
+++ VM/src/cpu.c
@@ -6,54 +6,59 @@
 /*   By: bbichero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/31 17:09:29 by bbichero          #+#    #+#             */
-/*   Updated: 2018/10/31 19:55:47 by bbichero         ###   ########.fr       */
+/*   Updated: 2018/11/01 16:24:02 by bbichero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../inc/vm.h"
 
-static int  ft_cycle_len(int opcode)
+static void   display_opt(t_vm_mem *vm, t_ps *ps)
 {
 	int i;
+	t_ps *lst;
 
-	i = -1;
-	while (++i < NB_OP)
+	i = 0;
+	lst = ps;
+	while (lst && ((i++) || true))
+		lst = lst->next;
+
+	DEBUG ? ft_printf("launching display_opt ...\n") : DEBUG;
+	if (vm->opt & DUMP && vm->cycle == vm->dump)
 	{
-		if (opcode == op_tab[i].opcode)
-			return (op_tab[i].cycle);
+		ft_prt_mem(vm, ps);
+		exit(ft_prt_winner(vm, ps));
 	}
-	return (1);
+	if (vm->opt & GRAPHIC && !(vm->cycle % vm->display))
+	{
+		ft_prt_mem(vm, ps);
+		ft_printf("cycles : %d - cycle_to_die : %d - nb_process : %d\n\n", vm->cycle, vm->cycle_to_die, i);
+	}
+	if (vm->opt & VERBOSE && !(vm->cycle % VERBOSE_DISPLAY))
+		ft_printf("cycles : %d - cycle_to_die : %d - nb_process : %d\n\n", vm->cycle, vm->cycle_to_die, i);
 }
 
 int   cpu(t_vm_mem *vm, t_ps *ps)
 {
-	t_ps	*lst;
+	t_ps	*lst_ps;
 	int		flag;
-	int		opcode;
-	int		cyc_len;
 
 	flag = false;
-	lst = NULL;
-	opcode = 0;
-	cyc_len = 0;
+	lst_ps = NULL;
 	DEBUG ? ft_printf("launching cpu ...\n") : DEBUG;
-	//prt_ps(ps the flag is used here to identify if the program has already passed
-	// the first cycle_to_die cycle  
-	while (flag == false && vm->cycle_to_die > 0 && cyc_len >= 0)
+	while ((flag == false ? true : ft_one_live_ps(ps)) && vm->cycle_to_die > 0)
 	{
-		ft_printf("cyc_len = %d\nopcode = %d\nflag = %d\n", cyc_len, opcode, flag);
-		if (cyc_len == 0)
-		{
-			opcode = *(vm->mem + MEM_CIR_POS(ps->pc));
-			cyc_len = ft_cycle_len(opcode);
-			if (!ft_one_live_ps(ps) || cyc_len == 1)
-				break ;
-			exec_op(vm, ps, lst, opcode, &flag);
-		}
-		cyc_len--;
+		if (!lst_ps)
+			lst_ps = ps;
+		cpu_checks(vm, ps);
+		display_opt(vm, ps);
+		flag = vm->cycle >= vm->cycle_to_die ? true : false;
+		DEBUG ? ft_printf("\nCPU => lst->playr : %s\ncycle : %d\ncycle_to_die : %d\n", lst_ps->playr, vm->cycle, vm->cycle_to_die) : DEBUG;
+		exec_op(vm, lst_ps);
+		vm->check++;
+		vm->ch_decr++;
 		vm->cycle++;
 	}
-	prt_ps(ps);
+	//prt_ps(ps);
 	//prt_vm(vm);
 	return (ft_prt_winner(vm, ps));
 }
--- VM/src/exec_op.c
+++ VM/src/exec_op.c
@@ -1,17 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vm.h                                               :+:      :+:    :+:   */
+/*   exec_op.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: igradea <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: bbichero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2013/10/04 11:33:27 by igradea               #+#    #+#         */
-/*   Updated: 2018/10/31 19:54:57 by bbichero         ###   ########.fr       */
+/*   Created: 2018/11/01 16:21:45 by bbichero          #+#    #+#             */
+/*   Updated: 2018/11/01 16:21:46 by bbichero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../inc/vm.h"
-
 
 static int  ft_valid_opcode(int opcode)
 {
@@ -24,32 +23,20 @@ static int  ft_valid_opcode(int opcode)
 	return (false);
 }
 
-static void   display_opt(t_vm_mem *vm, t_ps *ps)
+static int  ft_cycle_len(int opcode)
 {
 	int i;
-	t_ps *lst;
 
-	i = 0;
-	lst = ps;
-	while (lst && ((i++) || true))
-		lst = lst->next;
-	DEBUG ? ft_printf("launching display_opt ...\n") : DEBUG;
-	if (vm->opt & DUMP && vm->cycle == vm->dump)
+	i = -1;
+	while (++i < NB_OP)
 	{
-		ft_prt_mem(vm, ps);
-		exit(ft_prt_winner(vm, ps));
+		if (opcode == op_tab[i].opcode)
+			return (op_tab[i].cycle);
 	}
-	if (vm->opt & GRAPHIC && !(vm->cycle % vm->display))
-	{
-
-		ft_prt_mem(vm, ps);
-		ft_printf("cycles : %d - cycle_to_die : %d - nb_process : %d\n\n", vm->cycle, vm->cycle_to_die, i);
-	}
-	if (vm->opt & VERBOSE && !(vm->cycle % VERBOSE_DISPLAY))
-		ft_printf("cycles : %d - cycle_to_die : %d - nb_process : %d\n\n", vm->cycle, vm->cycle_to_die, i);
+	return (1);
 }
 
-static void   cpu_checks(t_vm_mem *vm, t_ps *ps)
+void   cpu_checks(t_vm_mem *vm, t_ps *ps)
 {
 	DEBUG ? ft_printf("launching cpu_checks ...\n") : DEBUG;
 	if (vm->check > vm->cycle_to_die)
@@ -73,50 +60,24 @@ static void   cpu_checks(t_vm_mem *vm, t_ps *ps)
 	}
 }
 
-int   exec_op(t_vm_mem *vm, t_ps *ps, t_ps *lst, int opcode, int *flag)
+int   exec_op(t_vm_mem *vm, t_ps *lst_ps)
 {
-	//ft_one_live_ps(ps)) && vm->cycle_to_die > 0)
-	//	ft_printf("cycles : %d - cycle_to_die : %d\n\n", vm->cycle, vm->cycle_to_die);
-	if (!lst)
-		lst = ps;
-	DEBUG ? ft_printf("\nCPU => lst->playr : %s\ncycle : %d\ncycle_to_die : %d\n", lst->playr, vm->cycle, vm->cycle_to_die) : DEBUG;
-	cpu_checks(vm, ps);
-	display_opt(vm, ps);
-	*flag = vm->cycle >= vm->cycle_to_die ? true : false;
-	lst = lst->next;
-	vm->check++;
-	vm->ch_decr++;
-
-	if (!ft_valid_opcode(opcode))
+	while (lst_ps && lst_ps->cyc_len >= 0)
 	{
-		ps->op_size = 1;
-		return (ft_next_op(ps, NO_CARRY));
+		if (lst_ps->cyc_len == 0)
+		{
+			lst_ps->opcode = *(vm->mem + MEM_CIR_POS(lst_ps->pc));
+			lst_ps->cyc_len = ft_cycle_len(lst_ps->opcode);
+			if (!ft_valid_opcode(lst_ps->opcode))
+			{
+				lst_ps->op_size = 1;
+				return (ft_next_op(lst_ps, NO_CARRY));
+			}
+			else
+				op_tab[OP_TAB_INDEX(lst_ps->opcode)].fun(vm, lst_ps, lst_ps->opcode);
+		}
+		lst_ps->cyc_len--;
+		lst_ps = lst_ps->next;
 	}
-	else
-		op_tab[OP_TAB_INDEX(opcode)].fun(vm, ps, opcode);
 	return (EXIT_SUCCESS);
-	/*
-	   int   opcode;
-	   int   cyc_len;
-
-	   DEBUG ? ft_printf("launching exec_op ...\n") : DEBUG;
-	   opcode = *(vm->mem + MEM_CIR_POS(ps->pc));
-	   cyc_len = ft_cycle_len(opcode);
-	   DEBUG ? (ft_valid_opcode(opcode) ? ft_printf("opcode : %d - op : %s\n", opcode, op_tab[OP_TAB_INDEX(opcode)].mmemo) : ft_printf("opcode : %d\n", opcode)) : DEBUG;
-	   while (cyc_len >= 0)
-	   {
-	   if (cyc_len == 0)
-	   {
-	   if (!ft_valid_opcode(opcode))
-	   {
-	   ps->op_size = 1;
-	   return (ft_next_op(ps, NO_CARRY));
-	   }
-	   else
-	   op_tab[OP_TAB_INDEX(opcode)].fun(vm, ps, opcode);
-	   }
-	   cyc_len--;
-	   }
-	   return (EXIT_SUCCESS);
-	   */
 }
