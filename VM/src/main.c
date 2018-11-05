3df9a5ca8e7debc205f8539ae645a8eef288babd
baptiste bicheron <bbichero@student.42.fr>
2018-11-05 22:12:39 +0100

    add clean comment and remove old one

--- VM/src/main.c
+++ VM/src/main.c
@@ -6,12 +6,18 @@
 /*   By: romontei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:33:27 by igradea           #+#    #+#             */
-/*   Updated: 2018/10/27 13:01:17 by romontei         ###   ########.fr       */
+/*   Updated: 2018/11/05 21:03:47 by bbichero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/vm.h"
 
+/*
+ *	ft_new_mem	= Instantiate VM
+ *	ft_parse_opt = parse cli arg
+ *	get_playr	= check argument players
+ *
+ */
 int				main(int ac, char **av)
 {
 	int			fd;
@@ -22,15 +28,11 @@ int				main(int ac, char **av)
 	ps = NULL;
 	if (ac < 2)
 		exit(ft_usage());
-	//Instantiate VM
 	vm = ft_new_mem();
-	// Parse cli arguments
 	ft_parse_opt(ac, av, vm);
-	// check argument player and add as much process there is players
 	get_playr(fd, &ps, ac, av);
 	add_data_vm(vm, ps);
 	cpu(vm, ps);
-	//DEBUGGING
 	//ft_main_debug(vm, ps);
 	//ft_prt_mem(vm, ps);
 	exit(EXIT_SUCCESS);
