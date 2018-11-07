476eaa6753a5ac88a0db290eb8e2864807f88bd7
baptiste bicheron <bbichero@student.42.fr>
2018-11-07 18:55:58 +0100

    file normed

--- ASM/src/ft_free.c
+++ ASM/src/ft_free.c
@@ -6,23 +6,23 @@
 /*   By: iongradea <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:43:01 by iongradea         #+#    #+#             */
-/*   Updated: 2013/11/06 14:44:35 by iongradea        ###   ########.fr       */
+/*   Updated: 2018/11/07 18:55:40 by bbichero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/asm.h"
 
-void  ft_free_head(t_header *head)
+void		ft_free_head(t_header *head)
 {
-  free(head->prog_name);
-  free(head->comment);
+	free(head->prog_name);
+	free(head->comment);
 }
 
-void  ft_free_all_inst(t_inst *inst)
+void		ft_free_all_inst(t_inst *inst)
 {
-  while (inst)
-  {
-    free(inst);
-    inst = inst->n;
-  }
+	while (inst)
+	{
+		free(inst);
+		inst = inst->n;
+	}
 }
