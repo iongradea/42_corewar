e82d202be5c3964a9b773f6acec2653f87169390
baptiste bicheron <bbichero@student.42.fr>
2018-11-07 20:47:12 +0100

    file normed

--- VM/src/main.c
+++ VM/src/main.c
@@ -6,18 +6,20 @@
 /*   By: romontei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:33:27 by igradea           #+#    #+#             */
-/*   Updated: 2018/11/06 19:43:27 by romontei         ###   ########.fr       */
+/*   Updated: 2018/11/07 20:46:39 by bbichero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/vm.h"
 
 /*
- *	ft_new_mem	= Instantiate VM
- *	ft_parse_opt = parse cli arg
- *	get_playr	= check argument players
- *
- */
+** ft_new_mem	= Instantiate VM
+** ft_parse_opt = parse cli arg
+** get_playr	= check argument players
+** ft_main_debug(vm, ps);
+** ft_prt_mem(vm, ps);
+*/
+
 int				main(int ac, char **av)
 {
 	int			fd;
@@ -33,7 +35,5 @@ int				main(int ac, char **av)
 	get_playr(fd, &ps, ac, av);
 	add_data_vm(vm, ps);
 	cpu(vm, ps);
-	//ft_main_debug(vm, ps);
-	//ft_prt_mem(vm, ps);
 	exit(EXIT_SUCCESS);
 }
