452c5cdd50c105ee9c51266e56fd2334edabadc8
baptiste bicheron <bbichero@student.42.fr>
2018-11-07 19:35:25 +0100

    file normed

--- ASM/src/parse_inst_2.c
+++ ASM/src/parse_inst_2.c
@@ -6,15 +6,15 @@
 /*   By: iongradea <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:43:01 by iongradea         #+#    #+#             */
-/*   Updated: 2018/11/06 20:16:59 by bbichero         ###   ########.fr       */
+/*   Updated: 2018/11/07 19:35:12 by bbichero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/asm.h"
 
-int 	ft_ch_opcode(char *str)
+int			ft_ch_opcode(char *str)
 {
-	int i;
+	int		i;
 
 	i = -1;
 	DEBUG ? ft_printf("launching ft_ch_opcode ...\n") : DEBUG;
@@ -26,9 +26,9 @@ int 	ft_ch_opcode(char *str)
 	return (false);
 }
 
-int		ft_ret_opcode(char *str)
+int			ft_ret_opcode(char *str)
 {
-	int i;
+	int		i;
 
 	i = -1;
 	DEBUG ? ft_printf("launching ft_ret_opcode ...\n") : DEBUG;
@@ -40,10 +40,10 @@ int		ft_ret_opcode(char *str)
 	return (ERROR);
 }
 
-int		ft_ch_err_lab(char *str)
+int			ft_ch_err_lab(char *str)
 {
-	int i;
-	int len;
+	int		i;
+	int		len;
 
 	i = -1;
 	len = ft_strlen(str);
@@ -56,9 +56,9 @@ int		ft_ch_err_lab(char *str)
 	return (false);
 }
 
-int 		ft_ch_rm_lab_c(char **str)
+int			ft_ch_rm_lab_c(char **str)
 {
-	int len;
+	int		len;
 
 	len = ft_strlen(*str);
 	DEBUG ? ft_printf("launching ft_ch_rm_lab_c ...\n") : DEBUG;
