299ba4635c161442895416e32716080c365fb39f
baptiste bicheron <bbichero@student.42.fr>
2018-11-07 18:45:42 +0100

    file normed

--- ASM/src/ft_annex.c
+++ ASM/src/ft_annex.c
@@ -6,43 +6,43 @@
 /*   By: iongradea <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:43:01 by iongradea         #+#    #+#             */
-/*   Updated: 2013/11/06 14:44:35 by iongradea        ###   ########.fr       */
+/*   Updated: 2018/11/07 18:45:15 by bbichero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/asm.h"
 
-int ft_is_valid_opcode(int opcode)
+int				ft_is_valid_opcode(int opcode)
 {
-  DEBUG ? ft_printf("launching ft_is_valid_opcode ...\n") : DEBUG;
-  if (opcode >= LIVE && opcode <= AFF)
-    return (true);
-  return (false);
+	DEBUG ? ft_printf("launching ft_is_valid_opcode ...\n") : DEBUG;
+	if (opcode >= LIVE && opcode <= AFF)
+		return (true);
+	return (false);
 }
 
-int ft_is_empty_line(char *line)
+int				ft_is_empty_line(char *line)
 {
-  int len;
-  int i;
+	int			len;
+	int			i;
 
-  i = -1;
-  DEBUG ? ft_printf("launching ft_is_empty_line ...\n") : DEBUG;
-  if (line == NULL)
-    return (true);
-  else
-    len = ft_strlen(line);
-  while (++i < len)
-    if (!ft_isspace(line[i]))
-      return (false);
-  return (true);
+	i = -1;
+	DEBUG ? ft_printf("launching ft_is_empty_line ...\n") : DEBUG;
+	if (line == NULL)
+		return (true);
+	else
+		len = ft_strlen(line);
+	while (++i < len)
+		if (!ft_isspace(line[i]))
+			return (false);
+	return (true);
 }
 
-char		*s_to_cor(char *str)
+char			*s_to_cor(char *str)
 {
-	int		i;
-	char	*new;
+	int			i;
+	char		*new;
 
-  DEBUG ? ft_printf("launching s_to_cor ...\n") : DEBUG;
+	DEBUG ? ft_printf("launching s_to_cor ...\n") : DEBUG;
 	if ((new = (char*)malloc(sizeof(char) * ft_strlen(str) + 3)) == NULL)
 		exit(0);
 	i = 0;
@@ -61,36 +61,36 @@ char		*s_to_cor(char *str)
 	return (new);
 }
 
-int   get_prog_comment(char *line, int *flag, t_header *head)
+int				get_prog_comment(char *line, int *flag, t_header *head)
 {
-  int comment_cmd_len;
+	int			comment_cmd_len;
 
-  DEBUG ? ft_printf("launching get_prog_comment ...\n") : DEBUG;
-  DEBUG ? ft_printf("LINE : %s\n", line) : DEBUG;
-  comment_cmd_len = ft_strlen(COMMENT_CMD_STRING);
-  if (ft_strnstr(line, COMMENT_CMD_STRING, comment_cmd_len))
-  {
-    ft_strcpy(head->comment, line + comment_cmd_len);
-    *flag = FL_COMMENT;
-  }
-  else
-    ft_strcpy(head->comment + ft_strlen(head->comment), line);
-  return (EXIT_SUCCESS);
+	DEBUG ? ft_printf("launching get_prog_comment ...\n") : DEBUG;
+	DEBUG ? ft_printf("LINE : %s\n", line) : DEBUG;
+	comment_cmd_len = ft_strlen(COMMENT_CMD_STRING);
+	if (ft_strnstr(line, COMMENT_CMD_STRING, comment_cmd_len))
+	{
+		ft_strcpy(head->comment, line + comment_cmd_len);
+		*flag = FL_COMMENT;
+	}
+	else
+		ft_strcpy(head->comment + ft_strlen(head->comment), line);
+	return (EXIT_SUCCESS);
 }
 
-int   get_prog_name(char *line, int *flag, t_header *head)
+int				get_prog_name(char *line, int *flag, t_header *head)
 {
-  int name_cmd_len;
+	int			name_cmd_len;
 
-  DEBUG ? ft_printf("launching get_prog_name ...\n") : DEBUG;
-  name_cmd_len = ft_strlen(NAME_CMD_STRING);
-  DEBUG ? ft_printf("LINE : %s\n", line) : DEBUG;
-  if (ft_strnstr(line, NAME_CMD_STRING, name_cmd_len))
-  {
-    ft_strcpy(head->prog_name, line + name_cmd_len);
-    *flag = FL_NAME;
-  }
-  else
-    ft_strcpy(head->prog_name + ft_strlen(head->prog_name), line);
-  return (EXIT_SUCCESS);
+	DEBUG ? ft_printf("launching get_prog_name ...\n") : DEBUG;
+	name_cmd_len = ft_strlen(NAME_CMD_STRING);
+	DEBUG ? ft_printf("LINE : %s\n", line) : DEBUG;
+	if (ft_strnstr(line, NAME_CMD_STRING, name_cmd_len))
+	{
+		ft_strcpy(head->prog_name, line + name_cmd_len);
+		*flag = FL_NAME;
+	}
+	else
+		ft_strcpy(head->prog_name + ft_strlen(head->prog_name), line);
+	return (EXIT_SUCCESS);
 }
