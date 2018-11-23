2ddd76ac6e71e75a43938f1255fb2b01a398f65b
Ion GRADEA <igradea@e1r3p3.42.fr>
2018-11-23 21:54:52 +0100

    ASM: testing script

---
+++ ASM_tst/asm_tst.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ASM="asm"
+EXEC_PATH="../ASM"
+EXEC_PATH_ZAZ="../vm_champs"
+SRC="../vm_champs/champs"
+DEST="src"
+DEST_ION="dst_ion"
+DEST_ZAZ="dst_zaz"
+
+mkdir $DEST 2> /dev/null || true
+mkdir $DEST_ION 2> /dev/null || true
+mkdir $DEST_ZAZ 2> /dev/null || true
+rm -rf $DEST/*
+rm -rf $DEST_ION/*
+rm -rf $DEST_ZAZ/*
+
+cp -r $SRC/* $DEST
+
+for f in $DEST/*.s ; do
+	cp "$f" $DEST_ION
+	cp "$f" $DEST_ZAZ
+done
+
+for f in $DEST/*/*.s ; do
+	cp "$f" $DEST_ION
+	cp "$f" $DEST_ZAZ
+done
+
+cp $EXEC_PATH/$ASM $DEST_ION
+cp $EXEC_PATH_ZAZ/$ASM $DEST_ZAZ
+
+for f in $DEST_ION/*.s ; do
+	echo "=====> ION |$f| <====="
+	$DEST_ION/$ASM "$f"
+done
+
+for f in $DEST_ZAZ/*.s ; do
+	echo "=====> ZAZ |$f| <====="
+	$DEST_ZAZ/$ASM "$f"
+done
