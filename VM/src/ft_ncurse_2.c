a9aa658d18fd94dad380069ab744d86d47d7f6da
baptiste bicheron <bbichero@student.42.fr>
2018-12-08 16:03:57 +0100

    add 2 new file for norme

---
+++ VM/src/ft_ncurse_2.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ncurse_2.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbichero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/08 15:56:06 by bbichero          #+#    #+#             */
+/*   Updated: 2018/12/08 16:03:16 by bbichero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/vm.h"
+
+void			ft_print_arena(t_vm_mem *vm, t_ps *ps)
+{
+	int			i;
+
+	i = 0;
+	erase();
+	ft_init_arena(vm);
+	while (i < MEM_SIZE)
+	{
+		if (ft_is_pc(ps, i))
+		{
+			vm->a[i].color != UNDEFINED ? \
+							  attron(COLOR_PAIR(vm->a[i].color_pc)) : true;
+			printw("%02x", 0xFF & vm->mem[i]);
+			vm->a[i].color != UNDEFINED ? \
+							  attroff(COLOR_PAIR(vm->a[i].color_pc)) : true;
+		}
+		else
+			ft_print_arena_2(vm, i);
+		printw(" ");
+		if ((i + 1) % MEM_LINE_LENGTH == 0)
+			printw("\n");
+		i++;
+	}
+	ft_print_game_stats(vm, ps);
+}
+
+void			ft_print_arena_2(t_vm_mem *vm, int i)
+{
+	vm->a[i].color != UNDEFINED ? attron(COLOR_PAIR(vm->a[i].color)) : true;
+	printw("%02x", 0xFF & vm->mem[i]);
+	vm->a[i].color != UNDEFINED ? attroff(COLOR_PAIR(vm->a[i].color)) : \
+					  true;
+}
+
+void			ft_ncurse(t_vm_mem *vm, t_ps *ps)
+{
+	t_ps		*tps;
+
+	tps = ps;
+	ft_init_ncurses();
+	ft_print_arena(vm, tps);
+}
