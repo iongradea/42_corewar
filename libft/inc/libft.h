441b5dc11016311f0bd49415dfccb57d89fa232a
Ion GRADEA <igradea@e1r5p19.42.fr>
2018-12-15 17:36:21 +0100

    libft: function to check if valid int

--- libft/inc/libft.h
+++ libft/inc/libft.h
@@ -6,7 +6,7 @@
 /*   By: igradea <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/16 16:21:07 by igradea           #+#    #+#             */
-/*   Updated: 2018/12/13 13:34:50 by bbichero         ###   ########.fr       */
+/*   Updated: 2018/12/15 17:02:26 by igradea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,6 +70,7 @@ char				*ft_strnstr(const char *big, const char *litt, size_t len);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_atoi(const char *str);
+int					ft_valid_int(const char *str);
 char				*ft_strnew(size_t size);
 void				ft_strdel(char **as);
 void				ft_strclr(char *s);
