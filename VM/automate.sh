b84d6371646206be81be6e7e9705b1d81d3755bf
baptiste bicheron <bbichero@student.42.fr>
2018-12-20 14:04:12 +0100

    all error are handle, default operand, ..

--- VM/automate.sh
+++ VM/automate.sh
@@ -1,21 +1,79 @@
 #!/bin/bash
 
-CYCLES=$1
+# Define color
+YEL='\033[1;33m'
+GRE='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
+USAGE="./automate.sh [cycle number] [champion path]"
+
+if [ ${#1} -eq 0 ]; then
+	echo -e ${RED}"you must provide a cycle number"${NC}
+	echo $USAGE
+	exit 1
+elif [ ${#2} -eq 0 ]; then
+	echo -e ${RED}"you must provide a champion"${NC}
+	echo $USAGE
+	exit 1
+elif [ ${#3} -gt 0 ]; then
+	echo -e ${RED}"you must provide 2 arguments"${NC}
+	echo $USAGE
+	exit 1
+elif [ ! -f "./corewar" ]; then
+	echo -e ${RED}"Corewar isn't in current dir"${NC}
+	read "corewar path: " input 
+	if [ -f input ]; then
+		echo -e ${RED}"Invalid dir, bye"${NC}
+		exit 1
+	fi
+fi
+
+# Take one arg, and int, return 1 if variable is an INT or 0 if not
+function	ft_is_int()
+{
+	# Check if value is numeric
+	if [ "$1" -eq "$1" ] 2>/dev/null
+	then
+		return 0
+	else
+		echo -e ${RED}"Value '`echo $1`' isn't numeric"${NC}
+		return 1
+	fi
+}
+
+# Check argument integrity
+if ! ft_is_int $1 ; then
+	exit 1
+fi
+
+if [ ! -f $2 ]; then
+	echo -e ${RED}"Value '`echo $2`' isn't a valid path"${NC}
+	exit 1
+fi
+
+CYCLES=$1
 CHAMP=$2
 
+# Execute corewar binary with dump option
 ./corewar -dump $CYCLES $CHAMP
 
 while true; do
-	echo $CYCLES
+	echo "Cycle: "$CYCLES
 
 	read input
 
 	MATCH=$(echo $input | grep -Eo '[+-][0-9]{1,}')
+	MATCH_OP=$(echo $input | grep -Eo '[+-]')
 
-	JUMP=$(echo $MATCH | cut -c 2-)
-
-	OPERAND=$(echo $MATCH | head -c 1)
+	if [ ${#MATCH} -ne 0 ]; then
+		JUMP=$(echo $MATCH | cut -c 2-)
+		OPERAND=$(echo $MATCH | head -c 1)
+	elif [ ${#MATCH_OP} -ne 0 ]; then
+		OPERAND=$input
+	elif [ ${#OPERAND} -eq 0 ] || [ $CYCLES -eq 1 ]; then
+		OPERAND=+
+	fi
 
 	if [[ $JUMP -gt 0 ]]; then
 		if [[ $OPERAND = "+" ]]; then
@@ -26,7 +84,7 @@ while true; do
 		if [[ $CYCLES -le 0 ]]; then
 			CYCLES=$((1))
 			./corewar -dump $CYCLES $CHAMP
-			echo "Cycles can't get under 0, cyka blyat..."
+			echo -e ${YEL}"Cycles can't get under 0, cyka blyat..."${NC}
 		else
 			./corewar -dump $CYCLES $CHAMP
 		fi
@@ -40,10 +98,16 @@ while true; do
 			if [[ $CYCLES -le 0 ]]; then
 				CYCLES=$((1))
 				./corewar -dump $CYCLES $CHAMP
-				echo "Cycles can't get under 0, cyka blyat..."
+				echo -e ${YEL}"Cycles can't get under 0, cyka blyat..."${NC}
 			else
 				./corewar -dump $CYCLES $CHAMP
 			fi
+		else
+			if [ ${#JUMP} -eq 0 ]; then
+				JUMP=100
+			fi
+			CYCLES=$(($JUMP $OPERAND $CYCLES))
+			./corewar -dump $CYCLES $CHAMP
 		fi
 	fi
 done
