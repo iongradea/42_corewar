4cad7aa736f585310f2da96001798eb64536d5b0
Ion GRADEA <igradea@e3r10p7.42.fr>
2019-01-08 18:02:39 +0100

    ASM: error if valid opcode and no parameter when no label

---
+++ ASM/tests/others/2_test.s
@@ -0,0 +1,6 @@
+.name "fork.s"
+.comment "fork"
+
+fork
+xxx2: live %42
+zjmp %:xxx2
--- libft/lbft_src/free.c
+++ libft/lbft_src/free.c
@@ -6,13 +6,13 @@
 /*   By: igradea <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/06 19:36:22 by igradea           #+#    #+#             */
-/*   Updated: 2018/12/13 13:33:46 by bbichero         ###   ########.fr       */
+/*   Updated: 2019/01/08 17:59:36 by igradea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/libft.h"
 
-void	ft_free_tab(char **tab)
+int		ft_free_tab(char **tab)
 {
 	int i;
 
@@ -23,6 +23,7 @@ void	ft_free_tab(char **tab)
 		i++;
 	}
 	free(tab);
+	return (1);
 }
 
 void	ft_free_tab_int(int **tab)
